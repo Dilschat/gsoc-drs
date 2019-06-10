54f9ae6d4ac6c14d8600aeb6d9a67bc01f667e91
Dilschat <dilschat@yandex.ru>
2019-06-10 20:56:37 +0500

    handler for object_id endpoint implemented

--- src/main/java/com/ega/datarepositorysevice/WebSecurityConfig.java
+++ src/main/java/com/ega/datarepositorysevice/WebSecurityConfig.java
@@ -1,6 +1,13 @@
 package com.ega.datarepositorysevice;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -9,6 +16,16 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class WebSecurityConfig{
 
+
+//    @Bean
+//    public MapReactiveUserDetailsService userDetailsService() {
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("user")
+//                .roles("USER")
+//                .build();
+//        return new MapReactiveUserDetailsService(user);
+//    }
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http.authorizeExchange().anyExchange().permitAll();
--- src/main/java/com/ega/datarepositorysevice/controller/handler/ObjectHandler.java
+++ src/main/java/com/ega/datarepositorysevice/controller/handler/ObjectHandler.java
@@ -1,21 +1,18 @@
 package com.ega.datarepositorysevice.controller.handler;
 
-import com.ega.datarepositorysevice.controller.HandlerUtils;
-import com.ega.datarepositorysevice.model.Error;
 import com.ega.datarepositorysevice.model.Object;
 import com.ega.datarepositorysevice.service.ObjectService;
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-import static com.ega.datarepositorysevice.controller.HandlerUtils.OBJECT_PATH_VARIABLE;
-import static com.ega.datarepositorysevice.controller.HandlerUtils.retrievePathVariable;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
-@Component
+@Service
 public class ObjectHandler {
     final static Logger logger = Logger.getLogger(ObjectHandler.class);
 
@@ -26,16 +23,19 @@ public class ObjectHandler {
         this.objectService = objectService;
     }
 
-    public Mono<ServerResponse> getObject(ServerRequest request) {
+    public Mono<ServerResponse> getObject(ServerRequest request){
+        String id = request.pathVariable("object_id");
+        System.out.println("id of object is: "+id);
+        logger.debug("id of object is: "+id);
+        Mono<ServerResponse> notFound = ServerResponse.notFound().build();
+        Mono<Object> objectMono = objectService.getObjectById(id);
+        System.out.println("Found "+objectMono.toString());
 
-        try {
-            Error notFoundError =  new Error("The requested Object wasn't found", HttpStatus.NOT_FOUND);
-            Mono<Object> objectMono = objectService
-                    .getObjectById(retrievePathVariable(request,OBJECT_PATH_VARIABLE));
-            return HandlerUtils.returnOkResponse(objectMono, notFoundError);
-        } catch (IllegalArgumentException e){
-            return HandlerUtils.returnBadRequest(e);
-        }
-
+        logger.debug("Found "+objectMono.toString());
+        System.out.println(objectMono.block().getName());
+        return objectMono.flatMap(user -> ServerResponse.ok()
+                .contentType(APPLICATION_JSON)
+                .body(BodyInserters.fromObject(user)))
+                .switchIfEmpty(notFound);
     }
 }
